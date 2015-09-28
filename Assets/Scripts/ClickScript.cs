e97c4cd12635199bd2b402c46ebd44e8ba2b75c2
Martin <kullax@feral.dk>
2015-09-28 11:18:17 +0200

    Initial Commit

---
+++ Assets/Scripts/ClickScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickScript : MonoBehaviour {
+    public GameObject target;
+    public LayerMask mask;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
+            {
+                if (target)
+                {
+                    target.SendMessage("Activate");
+                    Debug.Log("Hitting " + hit.collider.name);
+                }
+            }
+        }
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+<h1>Premise</h1>
+<p>You are an icecube using your environment to melt into water or steam trying to escape the sewers</p>
+
+<h1>Mechanics</h1>
+* Become water (through environment) to travel down narrow pipes.
+* Standard form is an ice cube which can push the cube by tapping it.
+
+<h1>Setting</h1>
+* Sewer pipes / kitchen pipes.
+* Sidescroller.
+
+<h1>Character</h1>
+* Ice cube.
+
+<h1>Control</h1>
+* The player controls the environment to help the ice cube.
+* Switches on environment which the player can tap to activate.
+
+<h1>Similar games</h1>
+* Cut the rope
