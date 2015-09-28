1eb55c6117a877c1509cd1a57edb03710361a37b
Martin <kullax@feral.dk>
2015-09-28 11:34:45 +0200

    More Initial Setup

---
+++ Assets/Editor/PerformBuild.cs
@@ -0,0 +1,52 @@
+﻿// C# example
+using UnityEditor;
+using System.IO;
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+class PerformBuild
+{
+	static string[] GetBuildScenes()
+	{
+		List<string> names = new List<string>();
+		
+		foreach(EditorBuildSettingsScene e in EditorBuildSettings.scenes)
+		{
+			if(e==null)
+				continue;
+			
+			if(e.enabled)
+				names.Add(e.path);
+		}
+		return names.ToArray();
+	}
+	
+	static string GetBuildPathAndroid()
+	{
+		return "Builds/Icescape.apk";
+	}
+	
+	[UnityEditor.MenuItem("CUSTOM/Test Command Line Build Step Android")]
+	static void CommandLineBuildAndroid ()
+	{
+		Debug.Log("Command line build android version\n------------------\n------------------");
+		
+		string[] scenes = GetBuildScenes();
+		string path = GetBuildPathAndroid();
+		if(scenes == null || scenes.Length==0 || path == null)
+			return;
+		
+		Debug.Log(string.Format("Path: \"{0}\"", path));
+		for(int i=0; i<scenes.Length; ++i)
+		{
+			Debug.Log(string.Format("Scene[{0}]: \"{1}\"", i, scenes[i]));
+		}
+
+		PlayerSettings.keystorePass = "pjaskevand";
+		PlayerSettings.keyaliasPass = "534231";
+		
+		Debug.Log("Starting Android Build!");
+		BuildPipeline.BuildPlayer(scenes, path, BuildTarget.Android, BuildOptions.None);
+	}
+}
