2bb3576f194c3f22ac74ad83964f81bd9979dc79
Thorbjørn S. Kaiser <lazareth@gmail.com>
2015-09-28 22:12:49 +0200

    add better debug options in inspector, fix distance calculation
    lesson learned: when fiddling around with implementing better debug for editor, you'll find bugs.

--- Assets/Scripts/GodMovable.cs
+++ Assets/Scripts/GodMovable.cs
@@ -3,13 +3,17 @@
 public class GodMovable : MonoBehaviour
 {
     public float DistanceReact = 1;
-    public bool ShowDistanceInEditor = true;
 
     public float HeldForce = 1;
     public ForceMode HeldForceMode = ForceMode.Force;
 
     public float TapForce = 1;
     public ForceMode TapForceMode = ForceMode.Impulse;
+
+    [Header("Debug options")]
+    public bool ShowDistanceInEditMode = true;
+    public bool ShowDistanceInPlayMode = false;
+    public bool ShowTouch = true;
 
     private GodPhase _phase;
     private Rigidbody _rb;
@@ -32,10 +36,10 @@ public class GodMovable : MonoBehaviour
 
 	void FixedUpdate ()
     {
-        if (_relativePosition == Vector3.zero || _relativePosition.magnitude > DistanceReact + transform.localScale.x)
+        if (_relativePosition == Vector3.zero || _relativePosition.magnitude > DistanceReact + transform.localScale.x/2)
             return;
 
-        var distMod = 1 - Mathf.Sqrt(_relativePosition.magnitude / (DistanceReact + transform.localScale.x));
+        var distMod = 1 - Mathf.Sqrt(_relativePosition.magnitude / (DistanceReact + transform.localScale.x/2));
         
         if (_phase == GodPhase.Began)
             _rb.AddForce(_relativePosition * distMod * TapForce, TapForceMode);
@@ -44,18 +48,31 @@ public class GodMovable : MonoBehaviour
             _rb.AddForce(_relativePosition * distMod * HeldForce, HeldForceMode);
 	}
 
+    private void DrawDebugDistance () {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, DistanceReact + transform.localScale.x/2);
+    }
+
+    private void DrawDebugTouch () {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(GodTouch.WorldPositionBegin, DistanceReact);
+    }
+
     void OnDrawGizmos() {
         if (!Application.isEditor)
             return;
+        
+        if (Application.isPlaying) {
+            if (ShowTouch && _phase != GodPhase.None)
+                DrawDebugTouch();
 
-        if (!Application.isPlaying && ShowDistanceInEditor) {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, DistanceReact + transform.localScale.x);
+            if (ShowDistanceInPlayMode)
+                DrawDebugDistance();
         }
 
-        if (Application.isPlaying && _phase != GodPhase.None) {
-            Gizmos.color = Color.blue;
-            Gizmos.DrawWireSphere(GodTouch.WorldPositionBegin, DistanceReact);
+        if (!Application.isPlaying) {
+            if (ShowDistanceInEditMode)
+                DrawDebugDistance();
         }
     }
 }
