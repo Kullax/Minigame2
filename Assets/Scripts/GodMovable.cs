78257abb68ac174ade525c36f6ec04d2928928ab
tskaiser <ngs604@alumni.ku.dk>
2015-09-28 17:24:15 +0200

    add GodMovable
    GodMovable is a physics script that use GodTouch to apply force relative to where the user tap in the world

---
+++ Assets/Scripts/GodMovable.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class GodMovable : MonoBehaviour
+{
+    public float distanceReact = 1;
+
+    public float heldForce = 1;
+    public ForceMode heldForceMode = ForceMode.Acceleration;
+
+    public float tapForce = 1;
+    public ForceMode tapForceMode = ForceMode.Impulse;
+
+    private GodPhase phase;
+    private Rigidbody rb;
+    private Vector3 relativePosition = Vector3.zero;
+
+    void Awake ()
+    {
+        rb = GetComponent<Rigidbody> ();
+        phase = GodTouch.phase;
+    }
+
+    void Update ()
+    {
+        phase = GodTouch.phase;
+
+        relativePosition = phase != GodPhase.None ?
+            transform.position - GodTouch.worldPositionBegin :
+            Vector3.zero;
+    }
+
+	void FixedUpdate ()
+    {
+        if (relativePosition == Vector3.zero || relativePosition.magnitude > distanceReact + transform.localScale.x)
+            return;
+
+        var distMod = 1 - Mathf.Sqrt(relativePosition.magnitude / (distanceReact + transform.localScale.x));
+        
+        if (phase == GodPhase.Began)
+            rb.AddForce(relativePosition * distMod * tapForce, tapForceMode);
+
+        if (phase == GodPhase.Held)
+            rb.AddForce(relativePosition * distMod * heldForce, heldForceMode);
+	}
+
+    void OnDrawGizmos()
+    {
+        if (!Application.isPlaying || phase == GodPhase.None)
+            return;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(GodTouch.worldPositionBegin, distanceReact);
+    }
+}
