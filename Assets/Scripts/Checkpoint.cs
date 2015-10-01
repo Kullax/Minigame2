27418f30be672ce0fd862cbe96b070c3dff66b56
tskaiser <ngs604@alumni.ku.dk>
2015-10-01 12:25:26 +0200

    Add GameManager script and rewire components to use this.

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
+    public GameRotation SpawnRotation;
+
     [Header("Debug options")]
     public bool DrawCheckpointInEditMode = true;
     public bool DrawCheckpointInPlayMode = false;
 
-    public void Spawn(GameObject prefab)
+    public GameObject Spawn(GameObject prefab)
     {
-        var obj = Instantiate(prefab, transform.position, transform.rotation);
+        GameManager.SetGameRotation(SpawnRotation);
+
+        return Instantiate(prefab, transform.position, SpawnRotation.AsReverseWorldQuaternion()) as GameObject;
     }
 
     public void SetThisAsActiveCheckpoint()
@@ -23,7 +27,7 @@ public class Checkpoint : MonoBehaviour {
         if (DrawCheckpointInEditMode && !Application.isPlaying || DrawCheckpointInPlayMode && Application.isPlaying)
         {
             Gizmos.color = Color.red;
-            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.matrix = Matrix4x4.TRS(transform.position, SpawnRotation.AsReverseWorldQuaternion(), Vector3.one);
             Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
             Gizmos.DrawFrustum(Vector3.zero, 15, 2, 1, 1);
         }
