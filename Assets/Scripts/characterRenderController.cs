c3d73e2f27d8fb1116a9321c2fa09daabd7a648e
tskaiser <ngs604@alumni.ku.dk>
2015-10-05 18:41:27 +0200

    fixed gimbal lock...

--- Assets/Scripts/characterRenderController.cs
+++ Assets/Scripts/characterRenderController.cs
@@ -5,20 +5,19 @@ public class characterRenderController : MonoBehaviour {
 
     private Camera MainCamera;
     private GameObject PlayerCube;
-    private Vector3 newAngle;
-    private Vector3 oldAngle;
-
-    // Use this for initialization
-    void Start () {
-
-	}
 
     private float xRotateVelocity = 0f;
     private float zRotateVelocity = 0f;
     private float rotateTime = 0.35f;
 
-	// Update is called once per frame
-	void LateUpdate () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
         PlayerCube = GameManager.CurrentPlayer;
         MainCamera = Camera.main;
 
@@ -40,40 +39,10 @@ public class characterRenderController : MonoBehaviour {
             xRot = 0;
             zRot = 0;
         }
-        
-        switch (GameManager.GameRotation) {
-            case (GameRotation.NegativeZ):
-            case (GameRotation.PositiveZ):
-                zRot = 0;
-                break;
-            case (GameRotation.NegativeX):
-            case (GameRotation.PositiveX):
-                xRot = 0;
-                break;
-        }
 
         xRot = Mathf.SmoothDampAngle(transform.eulerAngles.x, xRot, ref xRotateVelocity, rotateTime);
         zRot = Mathf.SmoothDampAngle(transform.eulerAngles.z, zRot, ref zRotateVelocity, rotateTime);
 
         transform.eulerAngles = new Vector3(xRot, yRot, zRot);
-
-        //Tild the rendered image on basis of the rigidbody
-        
-       // oldAngle = transform.localRotation.eulerAngles;
-       // newAngle = PlayerRB.transform.localRotation.eulerAngles;
-
-        /*if (GameManager.GameRotation == GameRotation.PositiveZ || GameManager.GameRotation == GameRotation.NegativeZ)
-        {
-            if (PlayerCube.GetComponent<Rigidbody>().velocity.y < -0.1)
-            {
-                transform.Rotate(new Vector3(0, 0, PlayerCube.transform.rotation.eulerAngles.z));
-            }
-        }
-        else
-        {
-            transform.Rotate(new Vector3(PlayerCube.transform.rotation.eulerAngles.z, 0, 0));
-        }
-
-        oldAngle = newAngle;*/
     }
 }
