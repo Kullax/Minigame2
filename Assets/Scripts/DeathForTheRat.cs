62d80dcb70558d80e3e9b9114e41b87547f12255
lemanou <lemanousakis@gmail.com>
2015-10-06 17:29:12 +0200

    test 1

---
+++ Assets/Scripts/DeathForTheRat.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathForTheRat : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Rat")
+        {
+            //Debug.Log("Rat point reached and destroyed!");
+            Destroy(col.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject SpawnGO,
+        StartGO,
+        EndGO;
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag != "Player")
+            return;
+
+        Instantiate(SpawnGO, StartGO.transform.position, StartGO.transform.rotation);
+
+        SetTarget tmp = FindObjectOfType<SetTarget>();
+        tmp.GetComponent<SetTarget>().EndTarget(EndGO.transform.position);
+
+        Destroy(this.gameObject);
+    }
+}
