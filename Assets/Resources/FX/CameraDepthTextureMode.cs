1bcee4e924970c40d3e5efcf6356cf133bf3b8fb
Octuki <derkonai@gmail.com>
2015-10-06 10:30:14 +0200

    All FX's

---
+++ Assets/Resources/FX/CameraDepthTextureMode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraDepthTextureMode : MonoBehaviour
+{
+    public DepthTextureMode mode;
+	void Start()
+    {
+        GetComponent<Camera>().depthTextureMode = mode;
+	}
+	
+	void OnValidate()
+    {
+        GetComponent<Camera>().depthTextureMode = mode;
+    }
+}
