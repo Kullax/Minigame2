d5461469af1ca81d1a683a5d1d9926dea9e4ba98
Martin <kullax@feral.dk>
2015-10-14 20:30:03 +0200

    What?

---
+++ Assets/FindCamerScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindCamerScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (!GetComponent<Canvas>().worldCamera)
+        {
+            GetComponent<Canvas>().worldCamera = Camera.main;
+            return;
+        }
+    }
+}
