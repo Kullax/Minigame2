1e32c95023b385b6d326ae64643f0cd5d9ee2bc8
ChristofferL <chrwadum@gmail.com>
2015-10-14 07:35:26 +0200

    Tilt controller

---
+++ Assets/Scripts/TiltController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiltController : MonoBehaviour {
+
+	public float Movement = 100;
+	[Range(0f,1.0f)]
+	public float Threshold = 0.2f;
+
+	private Rigidbody rb;
+	
+	// Update is called once per frame
+	void Update () {
+		if(rb == null)
+			rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+		if(Mathf.Abs(Input.acceleration.x) > Threshold){
+			rb.AddForce(Camera.main.transform.right * Input.acceleration.x * Movement);
+		}
+	}
+}
